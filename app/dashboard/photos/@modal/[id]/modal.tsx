11858f8aa0c6b247cd13ccefa7de339d9f75e1c7
Arden Sinclair <general@ardensinclair.com>
2023-10-23 09:38:30 -0700

    Fix image scaling on small screens

--- app/dashboard/photos/@modal/[id]/modal.tsx
+++ app/dashboard/photos/@modal/[id]/modal.tsx
@@ -11,11 +11,11 @@ export default function ImageModal(props: Props) {
 	const router = useRouter();
 
 	return (
-		<>
-			<div onClick={() => router.back()} className="fixed inset-0 z-30 bg-black bg-opacity-70" />
-			<div className="pointer-events-none fixed inset-8 z-30 flex items-center justify-center">
-				<img src={props.src} alt="User submitted image" className="pointer-events-auto h-full" />
-			</div>
-		</>
+		<div
+			onClick={() => router.back()}
+			className="fixed inset-0 z-30 flex items-center justify-center bg-black bg-opacity-70 p-8 backdrop-blur"
+		>
+			<img src={props.src} alt="User submitted image" className="h-full object-contain" />
+		</div>
 	);
 }
