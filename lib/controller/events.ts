7688cdfec07edebffec7c4ea22f3709b1a38b80e
Starry5464 <114651417+Starry5464@users.noreply.github.com>
2023-11-10 12:32:07 -0800

    Create events.ts

---
+++ lib/controller/events.ts
@@ -0,0 +1,32 @@
+import { db } from "../db";
+import { Event } from "../schema/event";
+
+export const addEvent = async ({description, startDate, endDate, name, venue, organiser,
+category} : Omit<Event, "id"> ): Promise<Event> => {
+
+	const [events] = await db.sql<[Event?]>`
+	INSERT INTO events
+	  (description, startDate, endDate, name, venue, organiser, category)
+	VALUES
+	  (${description}, ${startDate}, ${endDate}, ${name}, ${venue}, ${organiser},
+	  ${category})
+	RETURNING
+	  description,
+		startDate,
+		endDate,
+		name,
+		venue,
+		organiser,
+		category,
+	  id
+`;
+
+
+if (!events) {
+	throw new Error("Was unable to insert an event. No event returned");
+}
+
+return events;
+};
+
+
